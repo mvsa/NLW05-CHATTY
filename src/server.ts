edb72ea12d1bc727f4f5118e083c39cfe1f38681
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-04-21 01:45:40 -0300

    Setting up project

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (request, response)=>{
+    return response.send("Opa");
+})
+
+app.post("/", (request,response)=>{
+    return response.json({
+        message: "Salvo"
+    })
+})
+
+app.listen(3333, ()=>{
+    console.log("Server running on port 3333")
+});
