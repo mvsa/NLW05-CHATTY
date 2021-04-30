61a2550f02fb2b890603383c18f956cfe819c3e6
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-04-30 00:41:16 -0300

    setting up typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import express from "express";
 
+import "./database";
+
+import {routes} from "./routes";
+
 const app = express();
 
+app.use(express.json());
+app.use(routes);
 
-app.get("/", (request, response)=>{
-    return response.send("Opa");
-})
-
-app.post("/", (request,response)=>{
-    return response.json({
-        message: "Salvo"
-    })
-})
 
 app.listen(3333, ()=>{
     console.log("Server running on port 3333")
