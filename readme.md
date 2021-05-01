5d3a1cd6bcae740da53021e61531d202083c1f8c
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-05-01 19:49:53 -0300

    Finishing entities configuration

---
+++ readme.md
@@ -0,0 +1,11 @@
+Projeto Chatty desenvolvido na NLW 05
+
+Utiliza tecnologias: Node.js (Express), Typescript,  Typeorm
+
+
+Trabalho realizado nas aulas:
+- 1° Aula: Configuração inicial de ambiente. Instalação de principais pacotes (Express, Typescript)
+- 2° Aula: Instalação e configuração do TypeOrm. Utilização do Sqlite. Definição de principais Models, criação de migrations. Conceitos de Rotas, controllers e repositórios.
+- 3° Aula: Refatoração de código com adição de conceito de Services. Adição de novas entidades. Definição de relações entre as entidades
+- 4° Aula: Em andamento
+- 5° Aula: Em adamento
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate{
+    chat: boolean;
+    username: string;
+}
+
+class SettingsService {
+
+    private settingsRepository: Repository<Setting>
+
+    constructor(){
+        this.settingsRepository = getCustomRepository(SettingsRepository)
+    }
+
+    async create({chat, username} : ISettingsCreate){
+
+      
+
+        const userAlreadyExistis = await this.settingsRepository.findOne({username});
+
+        if (userAlreadyExistis){
+            throw new Error ("User already Exists!");
+        }
+    
+        const settings = this.settingsRepository.create({
+            chat,
+            username
+        }); 
+    
+        await this.settingsRepository.save(settings);
+
+        return settings;
+    }
+
+}
+
+export {SettingsService}
